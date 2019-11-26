cda4de8e7ea6f26af5f153e3fba91f90baf5ca42
GD7 <GD7@DESKTOP-QTO0NI0>
2019-11-26 15:13:14 +0900

    Servlet 기본 설정 하기

--- src/kr/gudi/Index.java
+++ src/kr/gudi/Index.java
@@ -7,9 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Index")
+//@WebServlet("/Index")
 public class Index extends HttpServlet {
-       
+      
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().print("Hi~~");
 	}
