fcfba7d7e71d6bdb320a69f9f238f1ef78442716
GD7 <GD7@DESKTOP-5RQ9K3J>
2019-12-16 11:14:21 +0900

    로컬 저장소 이용한 방법 연습

--- WebContent/Test2/test2.js
+++ WebContent/Test2/test2.js
@@ -1,3 +1,5 @@
+var resource = {};
+
 function drag(e){
 	e.dataTransfer.setData("src", e.target.src);
 }
@@ -6,7 +8,7 @@ function allow(e){
 	e.preventDefault(); 
 }
 
-function drop(e){
+function drop(e, target){
 	e.preventDefault();
 	var src = e.dataTransfer.getData("src");
 	var img = document.createElement("IMG");
@@ -14,10 +16,33 @@ function drop(e){
 	img.setAttribute("draggable", true);
 	img.setAttribute("ondragstart", "drag(event)");
 	e.target.appendChild(img);
+	
+	// 위치값 구하기
+	var children = target.parentNode.children;
+	var parent = target.parentNode;
+	for(var index = 0; index < children.length; index++){
+	  if(target == children[index]){
+	     resource[index] = src;
+	  }
+	}
+	
+	localStorage.setItem("grid", JSON.stringify(resource));
 }
 function rest(){
 	var size = document.getElementsByTagName("div").length;
 	for(var i = 0; i < size; i++){
 		document.getElementsByTagName("div")[i].innerHTML = "";
+	}
+}
+function reloadView(){
+	if(localStorage.grid){
+		resource = JSON.parse(localStorage.grid);
+		for(var key in resource){
+		   var img = document.createElement("IMG");
+			img.setAttribute("src", resource[key]);
+			img.setAttribute("draggable", true);
+			img.setAttribute("ondragstart", "drag(event)");
+		   document.getElementsByClassName("grid")[key].appendChild(img);
+		}
 	}
 }
