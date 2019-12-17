924558198ab8e9d4f24bb37b8b34f43724932a57
GD7 <GD7@DESKTOP-5RQ9K3J>
2019-12-17 10:00:28 +0900

    이미지 움직이게 하기 연습

--- WebContent/Test2/test2.js
+++ WebContent/Test2/test2.js
@@ -28,10 +28,12 @@ function drop(e, target){
 	
 }
 function rest(){
-	var size = document.getElementsByTagName("div").length;
+	var size = document.getElementsByClassName("grid").length;
 	for(var i = 0; i < size; i++){
-		document.getElementsByTagName("div")[i].innerHTML = "";
+		document.getElementsByClassName("grid")[i].innerHTML = "";
 	}
+	resource = {};
+	localStorage.grid = "";
 }
 function reloadView(){
 	if(localStorage.grid){
@@ -45,32 +47,58 @@ function reloadView(){
 		}
 	}
 }
-var x = 0; var y = 4;
-var i = 20;
+var x = 0; var y = 0;
+var i = 0;
 function keyEvent(e){
 //	console.log(e.keyCode);
+	var t = i;
 	switch (e.keyCode) {
-	case 37:  // 왼쪽
-		i--;
-		x--;
-		break;
-	case 38:  // 위쪽
-		i -= 5;
-		y--;
-		break;
-	case 39:  // 오른쪽
-		i++;
-		x++;
-		break;
-	case 40:  // 아래쪽
-		i += 5;
-		y++;
-		break;
-	default:
-		// 나머지
-		break;
+		case 37:  // 왼쪽
+			i--;
+			x--;
+			break;
+		case 38:  // 위쪽
+			i -= 5;
+			y--;
+			break;
+		case 39:  // 오른쪽
+			i++;
+			x++;
+			break;
+		case 40:  // 아래쪽
+			i += 5;
+			y++;
+			break;
+		default:
+			// 나머지
+			break;
 	}
 	
-	console.log(x, y, i);
+	if(x < 0) {
+		x = 0;
+		i++;
+	} else if(x > 4) {
+		x = 4;
+		i--;
+	}
+	
+	if(y < 0) {
+		y = 0;
+		i += 5;
+	} else if(y > 4) {
+		y = 4;
+		i -= 5;
+	}
+	
+	delete resource[t];
+	resource[i] = "http://localhost:8080/img/man.png";
+	localStorage.grid = JSON.stringify(resource);
+	reView();
 }
-
+function reView(){
+	var size = document.getElementsByClassName("grid").length;
+	for(var i = 0; i < size; i++){
+		document.getElementsByClassName("grid")[i].innerHTML = "";
+	}
+	reloadView();
+}
